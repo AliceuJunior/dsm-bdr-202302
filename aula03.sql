1237d06d9bfab43f55319392ff574e8d4ec56d4f
AliceuJunior <127908030+AliceuJunior@users.noreply.github.com>
2023-08-18 22:56:05 -0300

    Update aula03.sql

--- aula03.sql
+++ aula03.sql
@@ -1,8 +1,8 @@
 drop database if exists bd_aula03;
 create database bd_aula03;
 
-CREATE DOMAIN chk_status text CHECK (VALUE='disponivel' OR VALUE='indisponivel')
-CREATE DOMAIN categoria  text
+CREATE DOMAIN chk_status text CHECK (VALUE='disponivel' OR VALUE='indisponivel');
+CREATE DOMAIN chk_categoria  text (VALUE = 'DRAMA' or VALUE ='COMEDIA');
   
 create table tbl_cliente(
   codigo_cliente integer PRIMARY KEY,
@@ -18,12 +18,14 @@ create table tbl_emprestimo(
 create table tbl_titulo(
   codigo_titulo PRIMARY KEY,
   titulo varchar(40) NOT NULL,
-  descricao varchar(40)
+  descricao varchar(40),
+  categoria chk_categoria
 );
 create table tbl_livros(
   cod_livro integer PRIMARY KEY,
   codigo_titulo integer REFERENCES tbl_titulo(codigo_titulo),
-  status integer
+  status integer,
+  status chk_status
 );
 
 CREATE DOMAIN chk_status text CHECK (VALUE='disponivel' OR VALUE='indisponivel')
